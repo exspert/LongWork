48eb7e3f8906679afc520983ef13eee757c8f56c
exspert <exspert7777@gmail.com>
2017-10-19 16:29:16 +0300

    First push

---
+++ TestProject/TestProject/TestProject.cpp
@@ -0,0 +1,13 @@
+// TestProject.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("Kolka - suchka");
+	//bievkvhtgtg5t
+    return 0;
+}
+
