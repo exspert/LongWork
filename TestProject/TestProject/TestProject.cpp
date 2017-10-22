90c50b64fc1e975f72a5fcd1fe572de5d584962e
XPS13 <XPS13@DESKTOP-NGO54IA>
2017-10-22 20:48:09 +0300

    gitignore added

--- TestProject/TestProject/TestProject.cpp
+++ TestProject/TestProject/TestProject.cpp
@@ -1,8 +1,35 @@
-// KK
+// KK is megaproger
 
 #include "stdafx.h"
+#include "windows.h"
+
+int DisplayResourceNAMessageBox()
+{
+	int msgboxID = MessageBox(
+		NULL,
+		(LPCWSTR)L"Kolya cant gitignore\nDoes Kolya want to try again?",
+		(LPCWSTR)L"KK is megaproger",
+		MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+	);
+
+	switch (msgboxID)
+	{
+	case IDCANCEL:
+		// TODO: add code
+		break;
+	case IDTRYAGAIN:
+		// TODO: add code
+		break;
+	case IDCONTINUE:
+		// TODO: add code
+		break;
+	}
+
+	return msgboxID;
+}
 
 int main()
 {
-    return 0;
+	DisplayResourceNAMessageBox();
+	return 0;
 }
